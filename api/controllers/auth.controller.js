097089b9804014e3fd722197a9644bbfb82fc0c7
HimashaNethmini <himashanethmini05@gmail.com>
2024-04-09 20:02:24 +0530

    write a func to hashedPassword

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,13 @@
-export const register = (req,res) => {
+import bcrypt from "bcrypt";
+
+
+export const register = async (req,res) => {
     //these should be in the request body when registering a user
     const { username, email,password } = req.body;
 
     //hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword)
 
 
     //create new user & save to db
