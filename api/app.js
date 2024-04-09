07c0e7b3bce3b122af4a58ee732bbd74c6d9a88d
HimashaNethmini <himashanethmini05@gmail.com>
2024-04-09 19:42:27 +0530

    added authRoutes

--- api/app.js
+++ api/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import postRoute from "./routes/postRoute.js";
+import authRoute from "./routes/authRoutes.js";
 
 const app = express();
 
 app.use("/api/posts",postRoute);
+app.use("/api/auth", authRoute);
 
 
 app.listen(8800, () => {
--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -3,7 +3,7 @@ import { login, logout, register } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-//create post requests for register, login and logout under user 
+//post requests (create ) for register, login and logout under user 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
