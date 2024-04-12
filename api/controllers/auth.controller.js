992d635b310ba4fdc215873931d57f8ff6161da8
HimashaNethmini <himashanethmini05@gmail.com>
2024-04-12 11:11:05 +0530

    added error handling

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -6,19 +6,28 @@ export const register = async (req, res) => {
     //these should be in the request body when registering a user
     const { username, email, password } = req.body;
 
-    //hash password
-    const hashedPassword = await bcrypt.hash(password, 10);
-    console.log(hashedPassword)
+    try {
+
+        //hash password
+        const hashedPassword = await bcrypt.hash(password, 10);
+        console.log(hashedPassword)
 
 
-    //create new user & save to db
-    const newUser = await Prisma.user.create({
-        data: {
-            username,
-            email,
-            password: hashedPassword,
-        },
-    });
+        //create new user & save to db
+        const newUser = await Prisma.user.create({
+            data: {
+                username,
+                email,
+                password: hashedPassword,
+            },
+        });
+        console.log(newUser);
+
+        res.status(201).json({ message: "user created successfully" });
+
+    } catch (err) {
+        console.log(500).json({ message: "Failed to create user" });
+    }
 }
 
 export const login = (req, res) => {
